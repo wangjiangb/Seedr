74b09c9ba4bd320c3ce3aa5708e78c5ed4fac105
Jiang Wang <wangjiangb@gmail.com>
2012-02-06 15:53:29 -0600

    change home page

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Bigdata::Application.initialize!
 
-ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(
-  :default => '%d %b %Y'
-)
