866bd1a2b2f8b48c029ad4bfba536986932b15d3
Jiang Wang <wangjiangb@gmail.com>
2012-02-06 15:43:47 -0600

    add two pages

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Bigdata::Application.initialize!
+
+ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(
+  :default => '%d %b %Y'
+)
