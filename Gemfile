53cadef1d9eec15219e28142b75312ad89c4ace0
Jiang Wang <wangjiangb@gmail.com>
2012-02-15 11:13:14 -0600

    add search function

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.1.3'
+gem 'acts_as_indexed'
 
 # Bundle edge Rails instead:
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
@@ -16,6 +17,7 @@ gem 'formtastic-bootstrap'
 gem 'tabulous'
 
 gem 'json'
+
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/reader_controller.rb
+++ app/controllers/reader_controller.rb
@@ -1,6 +1,11 @@
 class ReaderController < ApplicationController
   def index
-    @news = LTweet.order("post_date DESC").limit(10).find(:all)
+    keywords = params[:keywords]
+    if keywords==nil
+      @news = LTweet.order("post_date DESC").limit(10).find(:all)
+    else
+      @news = LTweet.find_with_index(keywords,{:limit=>20,:order=>"post_date DESC"})
+    end
   end
 
   def setting
