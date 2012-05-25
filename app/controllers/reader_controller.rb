8d8e37e25f1c119e50b4d12d6db6669287c58112
Jiang Wang <wangjiangb@gmail.com>
2012-05-24 22:28:02 -0500

    add source selection

--- app/controllers/reader_controller.rb
+++ app/controllers/reader_controller.rb
@@ -102,8 +102,13 @@ class ReaderController < ApplicationController
       search_expression = "@weight*"+ (search_bin.weight_keyword/100).to_s() + "+ hasurl*"+search_bin.weight_hasurl.to_s()+"+num_of_retweets*"+search_bin.weight_retweet.to_s()+"+(post_date- Time.now)/3600*"+search_bin.weight_freshness.to_s();
 
       logger.info("search experssion:"+search_expression)
-      @news = LTweet.search(keywords, :match_mode => :boolean, :sort_mode=>:expr,:order=>search_expression,:page => params[:page],
-                               :per_page => 20)
+      if params[:source]=="All"
+      @news = LTweet.search(keywords, :match_mode => :boolean, :sort_mode=>:expr,:order=>search_expression,:page => params[:page],:per_page => 20)
+      else if params[:source]=="Twitter"
+             @news = LTweet.search(keywords, :match_mode => :boolean, :sort_mode=>:expr,:order=>search_expression,:page => params[:page],:per_page => 20,:conditions=>{:source=>1})
+      else
+             @news = LTweet.search(keywords, :match_mode => :boolean, :sort_mode=>:expr,:order=>search_expression,:page => params[:page],:per_page => 20,:conditions=>{:source=>0})
+      end
        # @news = LTweet.find_by_sql("SELECT t.* , a.screen_name, (k.weighting * [WEIGHTING_WITH_TYPE_1] + f.weighting * [WEIGHTING_WITH_TYPE_2] + hasurl * [WEIGHTING_WITH_TYPE_3] + retweet_count * [WEIGHTING_WITH_TYPE_4] + GREATEST((70 - datediff(CURDATE(), created_at)) / 70, 0) * [WEIGHTING_WITH_TYPE_5]) AS ultimateweighting FROM tweets t, friends_with f, accounts a, tmp_ranking k WHERE  f.parent = 23453742 AND f.child = t.twitter_id AND a.twitter_id = t.twitter_id AND t.tweet_id = k.tweet_id AND k.bin_id = {BIN_ID} ORDER BY ultimateweighting DESC LIMIT 0,250")
     end
     message_id = params[:message_id]
